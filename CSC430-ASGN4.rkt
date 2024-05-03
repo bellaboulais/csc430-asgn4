eb4210dc7cc49a09608b3c664eff4f33826145e7
bellaboulais <bjboulai@calpoly.edu>
2024-05-03 11:18:27 -0700

    updates

--- CSC430-ASGN4.rkt
+++ CSC430-ASGN4.rkt
@@ -14,7 +14,7 @@
 
 
 ; Define the ExprC language that utilizes a binop data type
-(define-type ExprC (U numC idC strC ifC locals lamb appC))                 
+(define-type ExprC (U numC idC strC ifC locals lamC appC))                 
 (tstruct numC ([n : Real]))                           
 (tstruct idC ([name : Symbol]))                          
 (tstruct strC ([str : String]))
@@ -23,42 +23,51 @@
 (tstruct lamC ([arg : Symbol] [body : ExprC]))
 (tstruct appC ([f : ExprC] [args : (Listof ExprC)]))
 
-(define-type Clauses (U clause clauses))
+(define-type Clauses (U clause clauses)) ; inside locals... ex. : add6 = {curradd 6} : 
 (tstruct clause ([id : Symbol] [expr : ExprC]))
 (tstruct clauses ([id : Symbol] [expr : ExprC] [rest : Clauses]))
 
-(define-type Value (U numV closV))
+(define-type Value (U numV boolV strV closV primV))
 (tstruct numV ([n : Number]))
-(tstruct closV ([arg : Symbol] [body : ExprC] [env : Env]))
+(tstruct boolV ([b : (U #t #f)]))
+(tstruct strV ([s : String]))
+(tstruct closV ([arg : Symbol] [body : ExprC] )) ; [env : Env]
+(tstruct primV ([p : Symbol]))
+
+; top level environemnt primitive that checks for +, -, *, /, <=, equal?, true, false, error v
+; need a eval prim helper function? parsed as appC and interpreted as a primitive
 
 
 ;; FUNCTIONS
 
-; top-interpt
+; top-interpt NOT COMPLETE
 (: top-interp (Sexp -> String)) 
 ;   PARAMS:  s : Sexp
 ;   RETURNS: String
 ;   PURPOSE:  
 (define (top-interp s)
-  (serialize (interp (parse s) top-env)))
+  "hi")
+ ; (serialize (interp (parse s) top-env)))
+
+(check-equal? (top-interp 'hi) "hi")
+
 
 
 ; interp
-(: interp (ExprC Environment -> Value))
+(: interp (ExprC -> Value))
 ;   PARAMS:   e:    ExprC           the expression to interpret
 ;             env:  Envrionment     list of functions defined in the current environment
 ;   RETURNS:  Value
 ;   PURPOSE:  
-(define (interp e env)
-  (match z                                                                     
-    [(numC n) n]                                                                
+(define (interp e)
+  (match e                                                                     
+    [(numC n) (numV n)]                                                                
     [(idC _) (error 'interp "ZODE: shouldn't get here")]
-    [(appC fun args) (local ([define fd (get-fundef fun fds)])                   
-                       (interp (subst args
-                                      (fdC-args fd)
-                                      (fdC-body fd))
-                               fds))]))   
-       
+    ))
+
+
+
+; (define-type ExprC (U numC idC strC ifC locals lamb appC))         
 ; parse  
 (: parse (Sexp -> ExprC))
 ;   PARAMS:   s :  Sexp
@@ -66,22 +75,36 @@
 ;   PURPOSE:  
 (define (parse s)
   (match s
-    [(list 'ifleq0? test then else) (ifleq0? (parse test) (parse then) (parse else))]
     [(? real? n) (numC n)]                            
     [(? symbol? s) (idC s)] ; variable reference cant be named operator 
-    [(list (? symbol? op) l r)
-     (if (hash-has-key? binop-table op)
-         (binop op (parse l) (parse r)) 
-         (appC op (list (parse l) (parse r))))]
-    [(list (? symbol? fun) args ...) 
-     (if (hash-has-key? binop-table fun)  
-         (error 'parse "ZODE: Invalid number of arguments")
-         (appC fun (map (lambda ([arg : Sexp]) (parse arg)) args)))]
+    [(? string? str) (strC str)]
+    
     
     [else (error 'parse "ZODE: Invalid expression")]))  
 
 
 
- 
+; parse test cases desugars locals into lambs?
+(check-equal? (parse 3) (numC 3))
+(check-equal? (parse 'x) (idC 'x))
+(check-equal? (parse "30") (strC "30"))
+
+
+(check-equal? (parse '{locals : x = 2 : y = 6 : {+ x y}})
+              (appC (lamC (clauses (idC 'x) (idC 'y))
+                          (appC (idC '+) (list (idC 'x) (idC 'y))))
+                    (list (numC 2) (numC 6))))
+              ;{{lamb : x y : {+ x y}} 2 6})
+
+(check-equal? (parse '{locals : z = {+ 9 14} : y = 98 : {+ z y}})
+              (appC (lamC (clauses (idC 'z) (idC 'y))
+                          (appC (idC '+) (list (idC 'z) (idC 'y))))
+                    (list (appC (idC '+) (list (numC 9) (numC 14))) (numC 98))))
+              ; {{lamb : z y : {+ z y}} {+ 9 14} 98})
+
+
+
+
+
 
 
